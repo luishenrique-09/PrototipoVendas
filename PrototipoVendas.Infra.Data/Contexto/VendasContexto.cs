1debecbedbd77c5aebcf4353747666697397532d
Luis Silva <luishenrique_09@hotmail.com>
2019-04-15 18:35:16 -0300

    Início do projeto.

---
+++ PrototipoVendas.Infra.Data/Contexto/VendasContexto.cs
@@ -0,0 +1,26 @@
+﻿namespace PrototipoVendas.Infra.Data.Contexto
+{
+    using Microsoft.EntityFrameworkCore;
+    using PrototipoVendas.Dominio.Entidades;
+    using PrototipoVendas.Infra.Data.Mapeamentos;
+    using System.Linq;
+
+    public class VendasContexto : DbContext
+    {
+        public VendasContexto(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+            
+            modelBuilder.ApplyConfiguration(new UsuarioMap());
+        }
+        
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
---
+++ PrototipoVendas.Web/Startup.cs
@@ -0,0 +1,65 @@
+﻿namespace PrototipoVendas.Web
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using PrototipoVendas.Infra.Data.Contexto;
+
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<VendasContexto>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("Prototipo")));
+
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
